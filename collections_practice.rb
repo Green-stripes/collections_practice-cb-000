a141f45bcddcb87dffd53bd0f00a080bad6d6dc3
Kaveri Bhattacharya <kavericb@hotmail.com>
2016-12-05 15:41:54 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,72 +1,57 @@
 require 'pry'
 
 def sort_array_asc(array)
-  array.sort
+copy = array.sort
+copy
 end
 
 def sort_array_desc(array)
-  array.sort do | left, right|
-    right <=> left
+  array.sort do |x, y|
+    y <=> x
   end
 end
 
 def sort_array_char_count(array)
-  array.sort do |left, right|
-    left.length <=> right.length
-  end
+array.sort do |a, b|
+  a.length <=> b.length
+end
 end
 
 def swap_elements(array)
-  array[1], array[2] = array[2], array[1]
-  array
+array = array[0], array[2] = array[2], array[1]
 end
 
 def reverse_array(array)
-  array.reverse
+array.reverse!
 end
 
 def kesha_maker(array)
   array.each do |item|
-    item[2] = "$"
-  end
+      item[2] = "$"
+    end
 end
 
 def find_a(array)
-  array.find_all do |word|
-    word[0] == "a"
+  new_arr = []
+  array.select do |x|
+    if x.start_with?("a")
+      new_arr << x
+    end
   end
-
-  # using select method
-    # array.select do |word|
-    #   word[0] == "a"
-    # end
+  new_arr
 end
 
 def sum_array(array)
-  sum = 0
-  array.each do |num|
-    sum+=num
-  end
-  sum
-
-  # using reduce method
-    # array.reduce(:+)
-
-  # using inject method (short)
-     # array.inject(:+)
-
-  # using inject method (long)
-     # array.inject do |sum,x|
-     #  sum + x
-     # end
+  array.inject {|a, b| a + b}
 end
 
 def add_s(array)
-  array.collect do |word|
-    if array[1] == word
-      word
-    else
-      word + "s"
-    end
+array.each_with_index.collect do |element, index|
+  if index == 1
+    element
+  else
+    element << "s"
   end
+end
+array
 end
